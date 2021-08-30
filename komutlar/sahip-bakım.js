ccbd22843dca11f3a0ce15e0e45cd99af454aaa0
Glitch (difficult-abundant-jasper) <none>
2021-08-30 13:38:33 +0000

    🐕🐆 Checkpoint ./komutlar/botlist-başvuru.js:35518859/5808 ./komutlar/botlist-botonayla.js:35518859/5997 ./komutlar/sahip-bakım.js:35518859/753

--- komutlar/sahip-bakım.js
+++ komutlar/sahip-bakım.js
@@ -2,19 +2,7 @@ const Discord = require('discord.js')
 const db = require("croxydb");
 const ayarlar = require('../ayarlar.json')
 exports.run = (client, message, args) => {
-let prefix = ayarlar.prefix
-      if(db.fetch(`bakim`)) {
-  const bakim = new Discord.MessageEmbed()
-  .setColor("#f6ff00")
-.setThumbnail(message.author.displayAvatarURL({dynamic : true}))
-  .setTitle('Üzgünüm Bot Bakımda')
-  .addField('Bot Şuan Bakımdadır Lütfen Bekleyin.','Bot Ne Durumda Yada Botla İlgili Güncelleme Ve Duyurular İçin Destek Sunucumuza Gelmeyi Unutmayınız.')
-  .addField('İşte Destek Sunucum',"[Destek Sunucusu](https://discord.gg/kqaBAxkkuX)")
-  .setFooter('Üzgünüm...')
-  .setImage('https://lh3.googleusercontent.com/proxy/gAN4I19oqqabXd_VIiwg5or-ITh4XxJTRNJA1ot0EIHPiBpxC74Atj4wNIcFes1N3VcE8WnOk6fIN29BChqNbj4lj9dIF2jiI7MBV6U8v842LA')
-if(message.author.id != "627803211348312065") return message.channel.send(bakim)
 
-}
   
   if(message.author.id != "627803211348312065") return //Bot sahip id
   
