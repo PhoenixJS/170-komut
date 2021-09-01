7ae97f93ac9867b816e0df57a29f14647e7da646
Glitch (difficult-abundant-jasper) <none>
2021-09-01 10:24:46 +0000

    ⛹🏎 Checkpoint ./index.js:35518859/1603

--- index.js
+++ index.js
@@ -1520,5 +1520,37 @@ client.on("roleCreate", async role => {
 })
 
 
- 
+ ///////////BOTA DM ATANLAR//////////////
+
+client.on("message", msg => {
+  var dm = client.channels.get("KANAL İD///////////BOTA DM ATANLAR//////////////
+
+client.on("message", msg => {
+  var dm = client.channels.get("KANAL İD");
+  if (msg.channel.type === "dm") {
+    if (msg.author.id === client.user.id) return;
+    const botdm = new Discord.RichEmbed()
+      .setTitle(`${client.user.username} Dm`)
+      .setTimestamp()
+      .setColor("RED")
+      .setThumbnail(`${msg.author.avatarURL}`)
+      .addField("Gönderen", msg.author.tag)
+      .addField("Gönderen ID", msg.author.id)
+      .addField("Gönderilen Mesaj", msg.content);
+
+    dm.send(botdm);
+  }
+  if (msg.channel.bot) return;
+})");
+  if (msg.channel.type === "dm") {
+    if (msg.author.id === client.user.id) return;
+    const botdm = new Discord.RichEmbed()
+      .setTitle(`${client.user.username} Dm`)
+      .setTimestamp()
+      .setColor("RED")
+      .setThumbnail(`${msg.author.avatarURL}`)
+      .addField("Gönderen", msg.author.tag)
+      .addField("Gönderen ID", msg.author.id)
+      .addField("Gönderilen Mesaj", msg.content);
+
 
