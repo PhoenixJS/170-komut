4ad77622959bfcd2d8af0578e7cf599a2b74376b
Glitch (difficult-abundant-jasper) <none>
2021-09-05 08:14:48 +0000

    👾🗺 Checkpoint ./komutlar/ekonomiyatir.js:35518859/3331 ./komutlar/ekonomisoygun.js:35518859/3282 ./komutlar/ekonomiçal.js:35518859/1436 ./komutlar/ekonomicalış.js:35518859/1999 ./komutlar/ekonomical.js:35518859/3828

---
+++ komutlar/ekonomical.js
@@ -0,0 +1,157 @@
+const Discord = require('discord.js');
+
+const ms = require('ms');
+
+const db = require('quick.db')//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+module.exports.run = async (client, message, args) => {
+
+  function rastgeleMiktar(min, max) {
+
+    min = Math.ceil(min);
+
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min)) + min;
+
+} 
+
+ let user =  message.mentions.users.first()
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+ if(!user) return message.channel.send(new Discord.MessageEmbed()
+
+                      .setColor("RED")
+
+                      .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))                   
+
+                      .setDescription(`⛔ Parasını çalmak istediğin kullanıcıyı etiketlemelisin!`))
+
+  let targetuser = await db.fetch(`para_${user.id}`);
+
+  let author     = await db.fetch(`çalma_${message.author.id}`);
+
+  let author2    = await db.fetch(`para_${message.author.id}`);
+
+  let timeout = 1200000;
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+if (author !== null && timeout - (Date.now() - author) > 0) {
+
+  let time = ms(timeout - (Date.now() - author));
+
+    let timeEmbed = new Discord.MessageEmbed()
+
+    .setColor("RED")
+
+    .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+
+     .setDescription(`⏱ Birisini soymak için ${time.minutes ? time.minutes + ' dakika,' : ''} ${time.seconds ? time.seconds + ' saniye beklemelisin!' : 'tekrar dene!'}`);
+
+    message.channel.send(timeEmbed)
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+  } else {
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+    let moneyEmbed = new Discord.MessageEmbed()
+
+  .setColor("RED")
+
+  .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))  
+
+  .setDescription(`⛔ Para çalmak için en az 200 💸 paraya ihtiyacın var`);
+
+  if (author2 < 200) {
+
+    return message.channel.send(moneyEmbed)
+
+  }//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+var lostmoney = rastgeleMiktar(150,500)
+
+  let moneyEmbed2 = new Discord.MessageEmbed()
+
+  .setColor("RED")
+
+  .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))  
+
+  .setDescription(`⛔ Faqir birisini soymaya çalıştın ve ${lostmoney} 💸 ceza yedin`);
+
+  
+
+  //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\ 
+
+  if (targetuser <= 100) {
+
+    return message.channel.send(moneyEmbed2),db.set(`çalma_${message.author.id}`, Date.now()),db.add(`para_$${message.author.id}`, -lostmoney)
+
+db.set(`çalma_${message.author.id}`, Date.now())
+
+db.add(`para_${message.author.id}`, -lostmoney)
+
+  }
+
+  let authorembed = new Discord.MessageEmbed()
+
+  .setColor("RED")
+
+  .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))  
+
+  .setDescription(`⛔ Kendini soymayımı düşünüyorsun?!`);
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+    if(user.id === message.author.id) {
+
+    return message.channel.send(authorembed)
+
+  }
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+    let gotmoney = rastgeleMiktar(600,1200)
+
+  let embed = new Discord.MessageEmbed()
+
+   .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))  
+
+   .setDescription(`✅ Soygun Başarılı, ${user}'ı soydun ve ${gotmoney} 💸 kazandın`)
+
+   .setColor("GREEN")
+
+   message.channel.send(embed)
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+    await db.add(`para_${user.id}`, -gotmoney);
+
+await db.add(`para_${message.author.id}`, gotmoney);
+
+await db.set(`çalma_${message.author.id}`, Date.now());
+
+  
+
+		}//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+}
+
+exports.conf = {
+
+  enabled: true,
+
+  aliases: [],
+
+};
+
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+
+exports.help = {
+
+  name: 'çal',
+
+};
